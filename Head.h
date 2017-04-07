0ceb543cb105a1474c562dcefe8666d54f3e2adc
Dmitriy Kuznetsov <dmitriykuznetsow@gmail.com>
2017-04-07 16:40:03 +0700

    Расческа

--- Head.h
+++ Head.h
@@ -16,13 +16,30 @@ using namespace std;
 		};
 		virtual void sort(){};
 	};
-	class Comb : public Sort
+class Comb : public Sort
 	{
 	public:
-		Comb()
-		{};
-		virtual void sort(){};
-	};	
+		Comb() : arr(0), N(0), K(0), time(0)
+		{
+		}
+		virtual void sort(int arr[], int size)
+		{
+			double fakt = 1.2473309;
+			int step = size - 1;
+			int x;
+			while (step > 1)
+			for (int i = 0; i + step < size; ++i)
+			{
+				K++;
+				if (arr[i]>arr[i + step])
+				{
+					swap(arr[i], arr[i + step]);
+					N++;
+				}
+				step /= fakt;
+			}
+		}
+	};
 	class Selection : public Sort
 	{
 	public:
@@ -33,4 +50,3 @@ using namespace std;
 		{
 		}
 	};
-}
